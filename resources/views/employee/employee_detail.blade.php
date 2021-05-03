e5cf96ecd3781478d74236736e36274f4822061a
Yericho <yericho25112001@gmail.com>
2021-05-03 11:58:51 +0700

    edited employee detail view

--- resources/views/employee/employee_detail.blade.php
+++ resources/views/employee/employee_detail.blade.php
@@ -8,15 +8,42 @@
     <div class="py-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="object-cover bg-cover h-60 w-full object-top bg-no-repeat" style="background-image: url('https://statik.tempo.co/data/2020/12/04/id_985339/985339_720.jpg')">
+                    <img class="inline-block h-52 w-52 rounded-full ring-2 ring white object-cover mt-32 ml-10" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80">
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200 ">
                     <div>
-                        <img class="inline-block h-52 w-52 rounded-full ring-2 ring white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80">
-                        <div class="w-full ml-5 mt-3">
-                            <div class="">
+                        <div class="ml-60 mt-6 mr-5">
+                            <div>
                                 <h6 class="font-bold text-2xl">{{ $details[0]->name }}</h6>
                             </div>
                             <div class="text-sm">
-                                <h2>{{ $details[0]->address }}</h2>
+                                <h2>{{ $details[0]->email }}</h2>
+                            </div>
+                            {{-- <div class="text-sm">
+                                <h2>{{ $details[0]->motto }}</h2>
+                            </div> --}}
+                        </div>
+                        <div class="mx-5 mt-3">
+                            <p class="font-bold text-lg">Detail</p>
+                            <table>
+                                <tr>
+                                    <td>Birth Date :</td>
+                                    <td>{{ $details[0]->birth_date }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Sex :</td>
+                                    <td>{{ $details[0]->sex }}</td>                                
+                                </tr>
+                                <tr>
+                                    <td>Address :</td>
+                                    <td>{{ $details[0]->address }}</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="mx-5 mt-5 bg-blue-400 h-96 flex flex-wrap content-center">
+                            <div class="w-full ">
+                                <p class="text-center">SHOW MAP</p>
                             </div>
                         </div>
                     </div>
