3b90617380ea9ed9e1c0ae4723c220c6eeab2066
Yericho <yericho25112001@gmail.com>
2021-05-03 14:17:36 +0700

    added function to display profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use Faker\Guesser\Name;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function profile(){
-
+        $id = Auth::user()->id;
+        $details = DB::table('users')->where('id','=',$id)->get();
+        return view('profile',['details'=>$details]);
     }
 
     public function profile_update() {
