d6c078bd083677c1070935bb674acfc75e91ee6a
EvanEdbert <evan.edbert@binus.ac.id>
2021-05-04 11:36:32 +0700

    new update on table

--- resources/views/company/company_detail.blade.php
+++ resources/views/company/company_detail.blade.php
@@ -10,8 +10,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="container">
-                    <div class="bg-cover h-44 w-full object-top bg-no-repeat mb-3" style="background-image: url('https://statik.tempo.co/data/2020/12/04/id_985339/985339_720.jpg')">
-                        <img class="float-left rounded-full h-44 w-44 object-cover ml-28 mt-28 mr-3" src="https://images.bisnis-cdn.com/posts/2021/03/27/1373332/mihoyo.jpg" alt="">
+                    <div class="bg-cover h-44 w-full object-top bg-no-repeat mb-2" style="background-image: url('https://statik.tempo.co/data/2020/12/04/id_985339/985339_720.jpg')">
+                        <img class="float-left rounded-full h-40 w-40 object-cover ml-20 mt-28 mr-3" src="https://images.bisnis-cdn.com/posts/2021/03/27/1373332/mihoyo.jpg" alt="">
                     </div>
                     <div>
                         <table>
@@ -28,13 +28,13 @@
                     </div>
                 </div>
                 <div class="py-8">
-                    <div class="float-left m-5 w-full">
-                        <span class="text-2xl"> Detail </span>
+                    <div class="float-left w-full">
+                        <span class="text-2xl ml-5"> Detail </span>
                         <button class="float-right mr-10" type="submit">
                              Save
                         </button>
                         <hr class="border border-5 border-black border-solid">
-                        <div class="overflow-scroll">
+                        <div class="overflow-auto ml-5 text-justify">
                             {{$detail->description}}
                         </div>
                     </div>
