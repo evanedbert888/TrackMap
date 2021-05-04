f035e235c9bd2289239e6b42865f45d8ec0794e1
Yericho <yericho25112001@gmail.com>
2021-05-04 17:07:45 +0700

    added new button style for save button

---
+++ resources/views/components/savebutton.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-yellow-400 border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest hover:bg-yellow-300 active:bg-yellow-500 focus:outline-none focus:border-yellow-500 focus:ring ring-yellow-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+    {{ $slot }}
+</button>
