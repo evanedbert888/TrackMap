c14091e01b2fbbf8b752aaa845039a2c2ce7c842
Yericho <yericho25112001@gmail.com>
2021-05-28 14:22:55 +0700

    added to update status user to verify

--- resources/views/Desktop/user_manage.blade.php
+++ resources/views/Desktop/user_manage.blade.php
@@ -5,21 +5,58 @@
         </h2>
     </x-slot>
     <script>
-        function showUser(){
-            alert("yericho");
-            // $.ajax({
-            //     url:'{{ route('show_user') }}',
-            //     method:"get",
-            //     success:function(data){
-            //         // setUserToTable(data);
-            //         console.log(data)
-            //     },
-            //     error:function(err){
-            //         console.log(err);
-            //     }
-            // })
-        }
+        $(function(e) {
+            $("#chkbxAll").click(function() {
+                $(".chkbx").prop('checked' ,$(this).prop('checked'));
+                
+                if ($('.chkbx:checked').length > 0) {
+                    $("#butverify").prop('disabled',false);
+                }
+                else {
+                    $("#butverify").prop('disabled',true);
+                }
+            })
+            
+        });
 
+        $(document).on('click', '.chkbx', function() {
+            if ($('.chkbx').length === $('.chkbx:checked').length) {
+                $('#chkbxAll').prop('checked', true);
+            }
+            else {
+                $('#chkbxAll').prop('checked', false);
+            }
+                
+            if ($('.chkbx:checked').length > 0) {
+                $("#butverify").prop('disabled',false);
+            }
+            else {
+                $("#butverify").prop('disabled',true);
+            }
+        })
+
+        function verifyUser() {
+            var listId = [];
+            $.each($("input:checkbox[name=ids]:checked"),function(){
+                listId.push($(this).val());
+            });
+
+            $.ajax({
+                url : '{{ route('update_status_user') }}',
+                type : 'POST',
+                data : {
+                    _token:$("input[name=_token]").val(),
+                    ids : listId
+                },
+                success:function(data){
+                    location.reload();
+                }, 
+                error:function(err){
+                    console.log(err);
+                }
+            });
+        }
+        
     </script>
 
     <div class="py-8">
@@ -28,13 +65,14 @@
                 <div class="p-5 bg-white border-b border-gray-200">
                     <div class="rounded-lg mt-3 p-3">
                         <div class="w-full">
+                        <div class="unverify">
                             <h1 class="text-2xl font-bold">Unverified</h1>
                             <div class="border border-black border-5 rounded rounded-full h-1 bg-black"></div>
                             <div class="flex mx-auto justify-center">
                                 <table class="w-full table-auto mt-4">
                                     <thead>
                                         <tr class="text-center text-base">
-                                            <th></th>
+                                            <th><input type="checkbox" id="chkbxAll" class="rounded border-black text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></th>
                                             <th> Name </th>
                                             <th> Birth Date </th>
                                             <th> Sex </th>
@@ -46,7 +84,7 @@
                                         @foreach($uvdusers as $user)
                                         <tr class="bg-red-100 border border-black border-b-2 border-t-0 border-r-0 border-l-0">
                                             <td>
-                                                <input value="{{ $user->id }}" id="verify" type="checkbox" class="rounded border-black text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="verify">
+                                                <input value="{{ $user->id }}" type="checkbox" name="ids" class="chkbx rounded border-black text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                                             </td>
                                             <td>{{ $user->name }}</td>
                                             <td>{{ $user->birth_date }}</td>
@@ -59,8 +97,9 @@
                                 </table>
                             </div>
                             <div class="flex mt-2 item-center justify-end">
-                                <x-savebutton class="font-bold">Verify</x-savebutton>
+                                <x-savebutton class="font-bold" id="butverify" onclick="verifyUser()" disabled>Verify</x-savebutton>
                             </div>
+                        </div>
                             <h1 class="text-2xl font-bold mt-8">Verified</h1>
                             <div class="border border-black border-5 border-b rounded rounded-full h-1 bg-black"></div>
                             <div class="flex mx-auto justify-center">
@@ -87,8 +126,8 @@
                                             <td>{{ $user->address }}</td>
                                             <td>{{ $user->email }}</td>
                                             <td>
-                                                <x-button>Edit</x-button>
-                                                <x-delbutton>Delete</x-delbutton>
+                                                <x-button value="{{ $user->id }}">Edit</x-button>
+                                                <x-delbutton value="{{ $user->id }}">Delete</x-delbutton>
                                             </td>
                                         </tr>
                                         @php
