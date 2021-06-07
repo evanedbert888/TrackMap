3b5764fd91d3890b1740cf977f9ef80cff654aa7
Yericho <yericho25112001@gmail.com>
2021-06-07 15:41:52 +0700

    added new relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,8 @@ class User extends Authenticatable
     public function registers() {
         return $this->hasMany(Register::class, 'user_id', 'id');
     }
+
+    public function goals() {
+        return $this->hasMany(Goal::class, 'user_id', 'id');
+    }
 }
