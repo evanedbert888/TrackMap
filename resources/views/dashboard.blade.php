1e1321d38e8815ceefe0e2adfd71514f51a9968d
Yericho <yericho25112001@gmail.com>
2021-06-16 14:26:30 +0700

    fixed error location at dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -47,105 +47,108 @@
                 latitude: value.latitude
             });
         })
-    }
 
-     require([
-        "esri/config",
-        "esri/Map",
-        "esri/views/MapView",
+        require([
+            "esri/config",
+            "esri/Map",
+            "esri/views/MapView",
 
-        "esri/Graphic",
-        "esri/layers/GraphicsLayer",
-        "esri/tasks/Locator",
-        "esri/layers/FeatureLayer",
+            "esri/Graphic",
+            "esri/layers/GraphicsLayer",
+            "esri/tasks/Locator",
+            "esri/layers/FeatureLayer",
 
 
-    ], function(esriConfig, Map, MapView, Graphic, GraphicsLayer, Locator, FeatureLayer, ) {
-        esriConfig.apiKey = "AAPKd14f6a7025a441bca958cfe373e9a0708Me2zOHz9-4bPzujZd2ZZkQ6W4n-UL8AB29QcugYNzzOh82WKuWHo1_Znivm110D";
-        const map = new Map({
-            basemap: "streets-navigation-vector" //Basemap layer service
-        });
-
-        const view = new MapView({
-            map: map,
-            center: [ 109.3425,  -0.0383],
-            zoom: 12,
-            container: "viewMap"
-        });
-
-        var graphics = places.map(function (place) {
-            return new Graphic({
-                attributes: {
-                    ObjectId: place.id,
-                    address: place.address,
-                    company: place.company,
-                    employee: place.employee
-                },
-                geometry: {
-                    longitude: place.longitude,
-                    latitude: place.latitude,
-                    type : "point"
-                }
+        ], function(esriConfig, Map, MapView, Graphic, GraphicsLayer, Locator, FeatureLayer, ) {
+            esriConfig.apiKey = "AAPKd14f6a7025a441bca958cfe373e9a0708Me2zOHz9-4bPzujZd2ZZkQ6W4n-UL8AB29QcugYNzzOh82WKuWHo1_Znivm110D";
+            const map = new Map({
+                basemap: "streets-navigation-vector" //Basemap layer service
             });
-        });
 
-        var featureLayer = new FeatureLayer({
-            source: graphics,
-            renderer: {
-                type: "simple",                    // autocasts as new SimpleRenderer()
-                symbol: {                          // autocasts as new SimpleMarkerSymbol()
-                    type: "simple-marker",
-                    color: "#102A44",
-                    outline: {                       // autocasts as new SimpleLineSymbol()
-                        color: "#598DD8",
-                        width: 2
+            const view = new MapView({
+                map: map,
+                center: [ 109.3425,  -0.0383],
+                zoom: 12,
+                container: "viewMap"
+            });
+
+            var graphics = places.map(function (place) {
+                return new Graphic({
+                    attributes: {
+                        ObjectId: place.id,
+                        address: place.address,
+                        company: place.company,
+                        employee: place.employee
+                    },
+                    geometry: {
+                        longitude: place.longitude,
+                        latitude: place.latitude,
+                        type : "point"
                     }
-                }
-            },
-            popupTemplate: {                     // autocasts as new PopupTemplate()
-                title: "Finished Place",    //belum dapat menampilan nama company
-                content: [{
-                    type: "fields",
-                    fieldInfos: [
-                        {
-                            fieldName: "address",
-                            label: "Company Address",
-                            visible: true
-                        },
-                        {
-                            fieldName: "employee",
-                            label: "Employee Name",
-                            visible: true
-                        }
-                    ]
-                }]
-            },
-            objectIdField: "ObjectID",           // This must be defined when creating a layer from `Graphic` objects
-            fields: [
-                {
-                    name: "ObjectID",
-                    alias: "ObjectID",
-                    type: "oid"
-                },
-                {
-                    name: "address",
-                    alias: "address",
-                    type: "string"
-                },
-                {
-                    name: "employee",
-                    alias: "employee",
-                    type: "string"
-                },
-                {
-                    name: "company",
-                    alias: "company",
-                    type: "string"
-                }
-            ]
-        });
+                });
+            });
 
-        map.layers.add(featureLayer);
-        
-    });
+            var featureLayer = new FeatureLayer({
+                source: graphics,
+                renderer: {
+                    type: "simple",                    // autocasts as new SimpleRenderer()
+                    symbol: {                          // autocasts as new SimpleMarkerSymbol()
+                        type: "simple-marker",
+                        color: "#102A44",
+                        outline: {                       // autocasts as new SimpleLineSymbol()
+                            color: "#598DD8",
+                            width: 2
+                        }
+                    }
+                },
+                popupTemplate: {                     // autocasts as new PopupTemplate()
+                    title: "Finished Place",    //belum dapat menampilan nama company
+                    content: [{
+                        type: "fields",
+                        fieldInfos: [
+                            {
+                                fieldName: "company",
+                                label: "Company Name",
+                                visible: true
+                            },
+                            {
+                                fieldName: "address",
+                                label: "Company Address",
+                                visible: true
+                            },
+                            {
+                                fieldName: "employee",
+                                label: "Employee Name",
+                                visible: true
+                            }
+                        ]
+                    }]
+                },
+                objectIdField: "ObjectID",           // This must be defined when creating a layer from `Graphic` objects
+                fields: [
+                    {
+                        name: "ObjectID",
+                        alias: "ObjectID",
+                        type: "oid"
+                    },
+                    {
+                        name: "address",
+                        alias: "address",
+                        type: "string"
+                    },
+                    {
+                        name: "employee",
+                        alias: "employee",
+                        type: "string"
+                    },
+                    {
+                        name: "company",
+                        alias: "company",
+                        type: "string"
+                    }
+                ]
+            });
+            map.layers.add(featureLayer); 
+        });
+    }
 </script>
