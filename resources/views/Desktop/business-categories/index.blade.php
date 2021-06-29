172d2cadab68893d5d1a28076fc8fd7328522fa1
EvanEdbert <evan.edbert@binus.ac.id>
2021-06-29 10:47:49 +0700

    new changes are made

--- resources/views/Desktop/business-categories/index.blade.php
+++ resources/views/Desktop/business-categories/index.blade.php
@@ -39,7 +39,7 @@
                                                 @csrf
                                                 @method("DELETE")
                                                 <div>
-                                                    <x-button type="button" onclick="showModal()"> Delete </x-button>
+                                                    <x-button onclick="showModal()"> Delete </x-button>
                                                 </div>
                                                 <div class="fixed z-10 inset-0 overflow-y-auto invisible opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" aria-labelledby="modal-title" role="dialog" aria-modal="true" id="modal">
                                                     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
