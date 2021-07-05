94d754eef352b814ae354c66818b3c1c88a93e61
EvanEdbert <evan.edbert@binus.ac.id>
2021-07-05 15:14:29 +0700

    modify the div body for session flash messages

--- resources/views/components/div-session.blade.php
+++ resources/views/components/div-session.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<div {{ $attributes->merge(['class' => 'text-lg font-semibold rounded-md p-2 mb-3']) }}>
+<div {{ $attributes->merge(['class' => 'text-lg font-semibold rounded-md p-2 mb-3 border-2 border-solid border-green-400']) }}>
     {{ $value ?? $slot}}
 </div>
