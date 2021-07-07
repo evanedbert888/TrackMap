9ebf2877e0078ec054f461be6e217448a59ff311
EvanEdbert <evan.edbert@binus.ac.id>
2021-07-07 15:13:40 +0700

    modify the div-session and create close-button component for session message

--- resources/views/components/div-session.blade.php
+++ resources/views/components/div-session.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<div {{ $attributes->merge(['class' => 'text-lg font-semibold rounded-md p-2 mb-3 border-2 border-solid border-green-400']) }}>
+<div {{ $attributes->merge(['class' => 'flex text-lg font-semibold rounded-md p-2 mb-3 border-2 border-solid border-green-400 justify-between items-center']) }}>
     {{ $value ?? $slot}}
 </div>
